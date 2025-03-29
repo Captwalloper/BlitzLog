157baf94f9c1e0805ffcca5957a00a4785d38eb7
Connor McCoy <comccoy@cfl.rr.com>
2025-03-29 04:35:51 -0400

    hopefully this fixes modder

--- keybind.py
+++ keybind.py
@@ -9,16 +9,17 @@ class Keybind:
         self.key = key
         self.bind = lambda _: bind()
         # Metaprogramming silliness to determine a nice display string for 'bind'
-        info = inspect.getframeinfo(inspect.getouterframes(inspect.currentframe())[1][0]).code_context[0].strip()
-        arg = info[info.find('(') + 1:-1].split(',')[1]
-        self.displayBind = arg
+        # info = inspect.getframeinfo(inspect.getouterframes(inspect.currentframe())[1][0]).code_context[0].strip()
+        # arg = info[info.find('(') + 1:-1].split(',')[1]
+        # self.displayBind = arg
 
     @staticmethod
     def SetupKeybinds(window: QMainWindow, keybinds: Iterable[Keybind], helpKey: str = None):
         def ShowKeybinds():
-            dlg = QDialog(window)
-            dlg.setWindowTitle("\n".join(map(lambda x: f'Key: {x.key}, Bind: {x.displayBind}', keybinds)))
-            dlg.exec()
+            # dlg = QDialog(window)
+            # dlg.setWindowTitle("\n".join(map(lambda x: f'Key: {x.key}, Bind: {x.displayBind}', keybinds)))
+            # dlg.exec()
+            pass
         if (helpKey != None):
             keybinds.append(Keybind(helpKey, ShowKeybinds))
         for keybind in keybinds:
